5dbe47416b1061f2981d3ff7bdad5739a941f854
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-02-28 21:44:50 +0100

    Init for the Google Hash Code 2018

---
+++ src/fr/alexisc/Main.java
@@ -0,0 +1,32 @@
+package fr.alexisc;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Parser
+        Parser parser = new Parser("res/big.in");
+
+        // Writer
+        PrintWriter writer = new PrintWriter("res/big.out");
+
+        // Core
+        String[] headers = parser.extractLine(" ");
+
+        Collection<Integer> collector = new ArrayList<>();
+        parser.applyToEachLines(line -> {
+            return line.length();
+        }, collector);
+
+        // Print the solution
+        writer.println(collector.toString());
+
+        // Close the writer
+        writer.close();
+    }
+}
---
+++ src/fr/alexisc/Parser.java
@@ -0,0 +1,59 @@
+package fr.alexisc;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.function.Function;
+
+/**
+ * Created by Alexis Couvreur on 25/02/2018.
+ */
+public class Parser {
+
+    private FileInputStream fileInputStream;
+    private BufferedReader bufferedReader;
+
+    public Parser(String fileName) throws FileNotFoundException {
+        fileInputStream = new FileInputStream(fileName);
+        bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+    }
+
+    public String[] extractLine(String delimiter) {
+
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line.split(delimiter);
+
+    }
+
+    /**
+     * Useful if you want to apply a method on each lines
+     * @param function that will process on each lines
+     * @param collector where the result ends up
+     * @param <T>
+     */
+    public <T> void applyToEachLines(Function<String, T> function, Collection<T> collector){
+        String line;
+
+        try {
+            while ((line = bufferedReader.readLine()) != null){
+                collector.add(function.apply(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void rewind(){
+        try {
+            fileInputStream.getChannel().position(0);
+        } catch (IOException e) {
+            System.out.println("The FileInputStream could not set the position 0 to the channel");
+        }
+        bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+    }
+
+}
